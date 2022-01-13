9bc99925ef350ec8a00e81e70e0e6fa9ae078556
Erick-Bravo <erickbrvo@gmail.com>
2022-01-12 20:11:11 -0800

    sayHello final

--- Exam/1-sayHello.js
+++ Exam/1-sayHello.js
@@ -7,29 +7,31 @@
 
 //If you'd like, call the functions in a log to see what they are outputting.
 
-//Only when you are ready to test this file, uncomment the module.exports below.
-//However, If you have not created all the functions yet, and the module.exports
-//are uncommented while you try to run node to see your logs, the terminal will throw
-// an error. This is because it freaks out when it cant find the function to export.
+//One more thing
+//When you are ready to test this file, uncomment the module.exports below. 
+//However, If you have not created all the functions yet, or
+//there is an unassigned variable (and the module.exports
+//are uncommented while you try to see your logs), the terminal will throw
+// an error. This is because it freaks out when it cant find the functions to export.
+
+
+
+
 
 
 //Problem 1
 //Write a function called sayHello and return a string of "Hello".
 
 
-const sayHello = () => {
- return "Hello"
-};
 
-console.log(sayHello());
 
 
 //Problem 2
 //Write a function called sayHelloTwo and return "Hello again, Mr. Bojangles"
 
-const sayHelloTwo = () => {
-    return "Hello again, Mr. Bojangles"
-};
+
+
+
 
 
 
@@ -38,10 +40,10 @@ const sayHelloTwo = () => {
 //module.exports section
 //As mentioned above, you will need to uncomment the below in order to pass the tests
 // If you don't remember how to do that, 
-// Highlight the module.exports and command + / (PC: windows + /)
+// Highlight the module.exports and hit command + / (PC: windows + /)
 // If you accidentally delete it, command + z (PC: windows + z) or re-write it.
 
-module.exports = {
-    sayHello,
-    sayHelloTwo
-};
+// module.exports = {
+//     sayHello,
+//     sayHelloTwo
+// };
