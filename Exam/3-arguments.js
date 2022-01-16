27a81816625a16f54ebdf81f1606ad5f3efef716
Erick-Bravo <erickbrvo@gmail.com>
2022-01-15 18:25:33 -0800

    arguments exam file

---
+++ Exam/3-arguments.js
@@ -0,0 +1,35 @@
+//Function with Arguments
+
+//Here you will write fuctions with arguments
+//Its like the last interpolatetion problem but backwards and writing annoying strings.
+//For the first four problems, you will just write in the arguments in the proper order
+//Then, you will write out the function completely.
+
+
+
+//Problem 1
+//Write in the appropriate arguments inside the interpolated string 
+//return "I think i'm tired of these stupid strings"
+
+const functionWithArgs = (arg1, arg2, arg3) => {
+    return `I think i'm ${arg2} of these ${arg3} ${arg1}`;
+};
+
+functionWithArgs("strings", "tired", "stupid");
+
+
+//Problem 2
+//Same as above
+//Modify the return so is looks like this: return bigger number > smaller number
+
+const functionWithArgsTwo = (num1, num2) => {
+    return num2 > num1;
+};
+
+functionWithArgsTwo(5, 24)
+
+//return 
+//Problem 3
+//Problem 4
+//Problem 5
+//Problem 6
